762902ba587b45abb1334ce9129ab1be18930ae3
exi <eternityexi@gmail.com>
2022-10-14 22:24:31 +0400

    fix .gitignore

--- commands/generate-images.js
+++ commands/generate-images.js
@@ -5,7 +5,7 @@ var directory = path.join(__dirname, '..', 'public', 'chart');
 var imageDirectory = path.join(__dirname, '..', 'public', 'img');
 var ChartToImage = require(path.join(__dirname, '..', 'utils', 'image'));
 
-fs.readdirSync(directory).forEach(year => {
+fs.readdirSync(directory).filter(file => !file === '.gitignore').forEach(year => {
   fs.readdirSync(path.join(directory, year)).forEach(month => {
     const files = fs.readdirSync(path.join(directory, year, month)).filter(file => file.endsWith('.json'));
     for (let file of files) {
