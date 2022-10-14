08b8c49e86237a65d7c0b6947574016be5dd3d5f
exi <eternityexi@gmail.com>
2022-10-14 22:25:51 +0400

    fix .gitignore

--- commands/generate-months.js
+++ commands/generate-months.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
 var directory = path.join(__dirname, '..', 'public', 'chart');
 var MonthWorker = require(path.join(__dirname, '..', 'utils', 'month'));
 
-fs.readdirSync(directory).filter(file => !file === '.gitignore').forEach(year => {
+fs.readdirSync(directory).filter(file => !(file === '.gitignore')).forEach(year => {
   const months = fs.readdirSync(path.join(directory, year));
   for (let month of months) {
     MonthWorker(path.join(directory, year, month));
