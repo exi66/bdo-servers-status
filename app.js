b9fa6fdbe03c3bfdb1c53058ba17d1a021f919fd
exi <eternityexi@gmail.com>
2022-10-14 22:44:43 +0400

    edit error handler

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : { status: 500, message: 'Internal error' };
+  res.locals.error = err;
 
   // render the error page
   res.status(err.status);
